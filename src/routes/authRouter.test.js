5cdb48b345b84b29af538fc7fd42f622f51b50c8
elsbels64 <wirth10@byu.edu>
2026-02-02 18:03:25 -0700

    add(authRouter.test.js) createAdminUser function

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,6 +1,17 @@
 const request = require('supertest');
 const app = require('../service');
 
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
 
