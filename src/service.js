3611cc990432877bf7e90005e6147649ded99c9f
elsbels64 <wirth10@byu.edu>
2026-04-02 01:52:15 -0600

    handle OPTIONS

--- src/service.js
+++ src/service.js
@@ -17,6 +17,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);  // ← handle preflight
+  }
   next();
 });
 
