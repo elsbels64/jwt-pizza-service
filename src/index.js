51c92abff8b1708da54e98e02fea8686df777602
elsbels64 <wirth10@byu.edu>
2026-04-02 15:58:51 -0600

    update port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const app = require('./service.js');
 
 const port = process.argv[2] || 3000;
-app.listen(port, '::', () => {
+
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server started on port ${port}`);
 });
